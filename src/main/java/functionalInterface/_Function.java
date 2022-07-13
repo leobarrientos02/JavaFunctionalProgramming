72a95af3f39acf9ce278b44e2bfac300aab8e55c
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-13 17:22:17 -0400

    Chaining functions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -10,10 +10,19 @@ public class _Function {
 
         Integer result2 = incrementByOneFunction.apply(1);
         System.out.println(result2);
-    }
 
+        int result3 = multiplyByTen.apply(result2);
+        System.out.println(result3);
+
+        Function<Integer, Integer> addOneANDMultiplyByTen =
+                incrementByOneFunction.andThen(multiplyByTen);
+        System.out.println(addOneANDMultiplyByTen.apply(4));
+    }
     static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;
+
+    static Function<Integer, Integer> multiplyByTen =
+            number -> number * 10;
     static int incrementByOne(int num){
         return num + 1;
     }
