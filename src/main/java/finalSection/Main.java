7c6ebb80cd2008896c6e3b9a07e36f09100a3d28
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-14 18:19:31 -0400

    Callback functions

---
+++ src/main/java/finalSection/Main.java
@@ -0,0 +1,52 @@
+package finalSection;
+
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Callback
+        hello("Leonel", "Barrientos", null);
+
+        hello("Jose", "Barrientos", value -> {
+            System.out.println("no lastname provided for " + value);
+        });
+
+        hello("Jose", null, value -> {
+            System.out.println("no lastname provided for " + value);
+        });
+
+        hello2("Jose", null, () -> {
+            System.out.println("no lastname provided");
+        });
+    }
+
+    // Callback function
+    static void hello(String firstName, String lastName, Consumer<String> callback) {
+        System.out.println(firstName);
+        if (lastName != null){
+            System.out.println(lastName);
+        } else {
+          callback.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable callback) {
+        System.out.println(firstName);
+        if (lastName != null){
+            System.out.println(lastName);
+        } else {
+            callback.run();
+        }
+    }
+
+    /* JavaScript
+    function helloWorld(firstName, lastname, callback) {
+        console.log(firstName);
+        if(lastName){
+            console.log(lastName);
+        }else{
+            callback();
+        }
+    }
+    */
+}
